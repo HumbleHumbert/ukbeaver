e5bc5069cfee51b67e45a6bd0fd8d5aee86150a0
HumbleHumbert <unrulytiger@163.com>
2025-11-25 21:09:38 +0800

    fix the pyproject

--- src/ukbeaver/data/imaging.py
+++ src/ukbeaver/data/imaging.py
@@ -1,5 +1,4 @@
 import polars as pl
-from polars import Int64, Float64, Utf8, Date, Categorical
 from pathlib import Path
 from collections import defaultdict
 from typing import Optional, Dict, Any, List, Tuple
@@ -13,40 +12,50 @@ class Imaging:
         img_dir: str,
     ) -> None:
 
-        sa = Schema()
-        self.table_1 = sa.get_schema(1)
-        self.table_3 = sa.get_schema(3)
-        self.table_13 = sa.get_schema(13)
-
         self.img_dir = img_dir
+
+        # Global cache dir
+        cache_dir = Path.home() / ".ukbeaver"
+        self.buffer_path = cache_dir / "parquets"
+        self.buffer_path.mkdir(exist_ok=True)
+
 
     def get_df(self,):
 
-        img_dir = self.img_dir
-        filenames = [f for f in os.listdir(img_dir)]
-        pattern = r"(?P<eid>\d+)_(?P<modality>\d+)_(?P<instance>\d+)_(?P<array>\d+)_(?:(?:\d+_)+)?(?P<specs>.+)$"
 
-        df = (pl.DataFrame({"filename": filenames})
-            .lazy()  # Use Lazy API for query optimization
-            .with_columns([
-                # Construct full path (string concatenation is fast in Polars)
-                (pl.lit(img_dir) + "/" + pl.col("filename")).alias("file_path"),
+        img_pqt = self.buffer_path / "img_info.parquet"
+        if img_pqt.exists():
+            return pl.read_parquet(img_pqt)
 
-                # Extract metadata using regex
-                pl.col("filename").str.extract_groups(pattern).alias("meta")
-            ])
-            .unnest("meta")  # Unpack the struct created by regex into columns
-            .with_columns([
-                # Cast numeric strings to Integers
-                pl.col("eid").cast(pl.Int64),
-                pl.col("modality").cast(pl.Int32),
-                pl.col("instance").cast(pl.Int32),
-                pl.col("array").cast(pl.Int32),
-                # specs remains string
-            ])
-            # --- LOGIC FOR DUPLICATES ---
-            .unique(subset=["eid", "modality", "instance", "array", "specs"], keep="first")
-            .collect() # Execute
-        )
+        else:
+            img_dir = self.img_dir
 
-        return df
+            filenames = [f for f in os.listdir(img_dir)]
+            pattern = r"(?P<eid>\d+)_(?P<modality>\d+)_(?P<instance>\d+)_(?P<array>\d+)_(?:(?:\d+_)+)?(?P<specs>.+)$"
+
+            df = (pl.DataFrame({"filename": filenames})
+                .lazy()  # Use Lazy API for query optimization
+                .with_columns([
+                    # Construct full path (string concatenation is fast in Polars)
+                    (pl.lit(img_dir) + "/" + pl.col("filename")).alias("file_path"),
+
+                    # Extract metadata using regex
+                    pl.col("filename").str.extract_groups(pattern).alias("meta")
+                ])
+                .unnest("meta")  # Unpack the struct created by regex into columns
+                .with_columns([
+                    # Cast numeric strings to Integers
+                    pl.col("eid").cast(pl.Int64),
+                    pl.col("modality").cast(pl.Int32),
+                    pl.col("instance").cast(pl.Int32),
+                    pl.col("array").cast(pl.Int32),
+                    # specs remains string
+                ])
+                # --- LOGIC FOR DUPLICATES ---
+                .unique(subset=["eid", "modality", "instance", "array", "specs"], keep="first")
+                .collect() # Execute
+            )
+
+            df.write_parquet(img_pqt)
+
+            return df
